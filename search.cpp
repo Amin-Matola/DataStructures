33ecf64482a47a96670c257620db5dacb1f44f6f
Amin <31763634+Amin-Matola@users.noreply.github.com>
2019-09-18 16:31:01 +0200

    Create search.cpp

---
+++ search.cpp
@@ -0,0 +1,22 @@
+//implement search algorithms with CPP
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+bool linear(int element, int store[]);
+bool binary(int element, int store[]);
+
+bool linear(int element, int store[]){
+      
+
+
+
+
+}
+
+bool binary(int element, int store){
+
+
+
+}
